ef9b8d29d797d1a6aebe9d36fd2b5c5673f80fbc
Nicolas Rendon Arias <nrendon87351@umanizales.edu.co>
2021-03-22 22:42:02 -0500

    Se crea la clase nodo de acuerdo con lo solicitado

---
+++ src/main/java/com/mycompany/listase/Nodo.java
@@ -0,0 +1,38 @@
+package com.mycompany.listase;
+
+/**
+ *
+ * @author nicorendon02
+ */
+public class Nodo {
+    public Object dato;
+    public Nodo siguiente;
+
+    public Nodo(Object dato, Nodo siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public String toString() {
+        return "Nodo{" + "dato=" + dato + ", siguiente=" + siguiente + '}';
+    }
+    
+    
+}
